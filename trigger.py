eaabe7a87b3051f431f3e6d76ee7075e3410159b
omeshvalyal <omeshvalyal@gmail.com>
2024-01-27 11:22:25 +0530

    new files are added

---
+++ trigger.py
@@ -0,0 +1,15 @@
+import requests
+
+# Assigning a URL to a variable
+package_Url = "https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.85/bin/apache-tomcat-9.0.85.tar.gz"
+
+# Making a GET request
+response = requests.get(package_Url)
+
+# Checking the response
+if response.status_code == 200:
+    print("Request successful")
+    print("Response content:")
+    print(response.text)
+else:
+    print("Request failed. Status code:", response.status_code)
