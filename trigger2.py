7ce8fc518bc8036c6e7a9126d3772fcfea4446df
omeshvalyal <omeshvalyal@gmail.com>
2024-01-27 19:09:07 +0530

    code has changed

---
+++ trigger2.py
@@ -0,0 +1,5 @@
+# Assigning a URL to a variable
+package_url = "https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.85/bin/apache-tomcat-9.0.85.tar.gz"
+
+# Print the package URL
+print("Package URL:", package_url)
