163982dc41f4bf247b8c7a7bf67449c4d91db09d
omeshvalyal <omeshvalyal@gmail.com>
2024-01-27 19:43:10 +0530

    code has changed

--- trigger2.py
+++ trigger2.py
@@ -3,3 +3,13 @@ package_url = "https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.85/bin/apache-tomca
 
 # Print the package URL
 print("Package URL:", package_url)
+# save this script as 'print_url.py' or any other name you prefer
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python print_url.py <url>")
+        sys.exit(1)
+
+    url_parameter = sys.argv[1]
+    print(f"The provided URL is: {url_parameter}")
